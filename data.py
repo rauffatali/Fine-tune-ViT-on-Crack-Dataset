40d015a1bc6bf1f7e65077d8674fe69d30f424a6
rauffatali <fetelirauf@gmail.com>
2024-06-06 12:36:09 +0400

    added functionality files

---
+++ data.py
@@ -0,0 +1,73 @@
+import os
+import random
+import pandas as pd
+from pathlib import Path
+
+from utils import is_imbalanced, prep_data
+
+def balance_data(data: dict) -> dict:
+    for dataset in data:
+        if is_imbalanced(data[dataset]):
+            class_counts = {cls: len(data[dataset][cls]) for cls in data[dataset]}
+            underrepresented, overrepresented = min(class_counts, key=class_counts.get), max(class_counts, key=class_counts.get)
+            data[dataset][overrepresented] = random.choices(data[dataset][overrepresented], k=len(data[dataset][underrepresented]))
+    
+    return data
+
+def get_data_as_dataframe(path: Path, dataset_name: str = None, balance: bool = False) -> pd.DataFrame:
+    """
+    This function reads data from a folder structure and returns a DataFrame for a dataset.
+
+    The function assumes the following directory structure:
+        path/
+        dataset_name/
+        class_1/
+        image1.jpg
+        image2.jpg
+        ...
+        class_2/
+        image3.jpg
+        image4.jpg
+        ...
+
+    Args:
+        path (Path): The base path of the folder containing datasets.
+        dataset_name (str, Optional): The name of the specific dataset to load (default is None).
+        balance (bool, Optional): A boolean flag indicating whether to perform class balancing on the data (default is False).
+
+    Returns:
+        A pandas DataFrame containing the loaded data or None if the dataset is not found.
+
+    Raises:
+        ValueError: If the folder structure is not as expected.
+    """
+    data = dict()
+    
+    if dataset_name:
+        if dataset_name not in os.listdir(path):
+            raise ValueError(f"Dataset {dataset_name} not found.")
+        else:
+            try:
+                data = prep_data(path, data, dataset_name)
+            except:
+                raise ValueError('Unknown folder structure.')
+    else:
+        try:
+            for d_name in os.listdir(path):
+                data = prep_data(path, data, d_name)
+        except:
+            raise ValueError('Unknown folder structure.')
+    
+    if balance:
+        data = balance_data(data)
+
+    images = []
+    labels = []
+
+    for _, class_data in data.items():
+        for _, image_data in class_data.items():
+            for image, label in image_data:
+                images.append(image)
+                labels.append(label)
+
+    return pd.DataFrame({'image': images, 'label': labels})
---
+++ utils.py
@@ -0,0 +1,87 @@
+import os
+import pandas as pd
+from pathlib import Path
+
+from sklearn.model_selection import train_test_split
+
+SEED = 42
+
+def custom_train_test_split(df: pd.DataFrame, test_size: float = 0.2, stratify=None) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    """
+    Splits a DataFrame into training, validation, and test sets while preserving class distribution (if applicable).
+
+    This function performs stratified train-test splitting on a DataFrame in a single step using scikit-learn's `train_test_split` function. 
+    It directly splits the data into three sets based on the provided `test_size`.
+
+    Args:
+        df (pd.DataFrame): The DataFrame containing the data to be split.
+        test_size (float, optional): The proportion of the data for the combined validation and test sets (default is 0.2).
+        stratify (array-like, optional): If provided, stratification is applied (maintains class distribution across splits). 
+
+    Returns:
+        pd.Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]: A tuple containing the training DataFrame, validation DataFrame, and test DataFrame.
+
+    Raises:
+        ValueError: If `test_size` is not between 0.0 and 1.0.
+    """
+
+    if not 0.0 <= test_size <= 1.0:
+        raise ValueError("test_size must be between 0.0 and 1.0")
+
+    validation_ratio = test_size / (1 - test_size)
+    if stratify:
+        train_df, valid_test_df = train_test_split(df, test_size=test_size, random_state=SEED, stratify=df[stratify].values)
+        valid_df, test_df = train_test_split(valid_test_df, test_size=validation_ratio, random_state=SEED, stratify=valid_test_df[stratify].values)
+    else:
+        train_df, valid_test_df = train_test_split(df, test_size=test_size, random_state=SEED, stratify=stratify)
+        valid_df, test_df = train_test_split(valid_test_df, test_size=validation_ratio, random_state=SEED, stratify=stratify)
+
+    return train_df, valid_df, test_df
+
+def is_imbalanced(data: dict, threshold_ratio: float = 1.2) -> bool:
+    """
+    Checks for binary class imbalance based on a ratio threshold.
+
+    Args:
+        data: A dictionary or list where keys or elements represent class labels.
+        threshold_ratio: The ratio between majority and minority class size to consider imbalance (default=1.5).
+
+    Returns:
+        True if class imbalance is detected, False otherwise.
+    """
+    if not data:
+        raise ValueError("Data dictionary cannot be empty")
+    
+    class_counts = {cls: len(data[cls]) for cls in data}
+    
+    majority_count = max(class_counts.values())
+    minority_count = min(class_counts.values())
+
+    if majority_count / minority_count > threshold_ratio:
+        return True
+    else:
+        return False
+
+def prep_data(path: Path, data: dict, dataset_name: str = None) -> dict:
+
+    """
+    Args:
+        path (Path): The path to the directory containing the dataset.
+        data (dict): A dictionary to store the preprocessed data. 
+        dataset_name (str, optional): The name of the dataset within the `data` dictionary (default is None).
+
+    Returns:
+        dict: The updated `data` dictionary with the preprocessed image data.
+
+    """
+
+    data[dataset_name] = dict()
+    for cls in os.listdir(os.path.join(path, dataset_name)):
+        data_cls_path = os.path.join(path, dataset_name, cls)
+        for filename in sorted(os.listdir(data_cls_path)):
+            if cls not in data[dataset_name]:
+                data[dataset_name][cls] = []
+            data[dataset_name][cls].append([os.path.join(data_cls_path, filename), cls])
+            break
+    
+    return data
